583a4b45a7464f7d2ba64a0e1e124d90a0b4d68f
Mariani88 <pm_mariani@hotmail.com>
2015-04-14 17:29:49 -0300

    pasa el segundo test

--- ejercicio1/src/ejercicio1/CharCounter.java
+++ ejercicio1/src/ejercicio1/CharCounter.java
@@ -2,11 +2,20 @@ package ejercicio1;
 
 public class CharCounter {
 
+	private String characters;
+	
 	public CharCounter (String characters){
-		
+		this.characters = characters;
 	}
 
 	public int howMany (char character){
-		return 0;
+		
+		int result = 0;
+		
+		if ( character == '$' ){
+			result = -1;
+		}
+		
+		return result;
 	}
 }
